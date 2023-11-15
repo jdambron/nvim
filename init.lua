3ec0e85be0753625a846398edfd29d33b7d5cf1b
Julien Dambron <dreamlike3d@gmail.com>
2023-11-15 15:31:06 +0100

    feat: change theme to catppuccin-frappe

--- init.lua
+++ init.lua
@@ -154,10 +154,11 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'catppuccin/nvim',
+    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin-frappe'
     end,
   },
 
@@ -168,7 +169,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin-frappe',
         component_separators = '|',
         section_separators = '',
       },
