d5c76e2b56a334b4bafd17b6c1a6b0b6936dd02b
Julien Dambron <dreamlike3d@gmail.com>
2023-11-22 08:17:05 +0100

    feat: relative line number + independent clipboards

--- init.lua
+++ init.lua
@@ -246,10 +246,13 @@ vim.wo.number = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
+-- Enable relative line number
+vim.wo.relativenumber = true
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
